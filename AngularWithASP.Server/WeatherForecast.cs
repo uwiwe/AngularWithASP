ceb26059d3f373a8b314189ca1f6b8829278641f
wiwe <wiweurdaneta@gmail.com>
2024-02-08 23:11:53 -0400

    Update

--- AngularWithASP.Server/WeatherForecast.cs
+++ AngularWithASP.Server/WeatherForecast.cs
@@ -11,15 +11,3 @@ namespace AngularWithASP.Server
         public string? Summary { get; set; }
     }
 }
-
-
-/*public class WeatherForecast
-{
-    public DateOnly Date { get; set; }
-
-    public int TemperatureC { get; set; }
-
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-    public string? Summary { get; set; }
-} */
