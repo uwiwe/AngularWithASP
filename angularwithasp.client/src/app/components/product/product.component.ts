8f347913dfe0a42c9dd390ed4f9a31f09f68af14
wiwe <wiweurdaneta@gmail.com>
2024-02-23 18:21:53 -0400

    New Component

---
+++ angularwithasp.client/src/app/components/product/product.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+
+  @Input() title: string = '';
+
+}
