6bc98c8f2f7b8fa6e701ac822174739b20325c36
wiwe <wiweurdaneta@gmail.com>
2024-02-24 17:50:36 -0400

    Update app.component.ts

--- angularwithasp.client/src/app/app.component.ts
+++ angularwithasp.client/src/app/app.component.ts
@@ -1,40 +1,15 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-
-  title = 'angularwithasp.client';
-
+export class AppComponent {
+  title = 'eCommerce-app'
+  http = inject(HttpClient); // voy a usar el httpclient
+  
   changeTitle() {
     this.title = 'Nombre Cambiado';
   }
