fccda61f955301803b33af0e5c8f92e6c5cb300a
wiwe <wiweurdaneta@gmail.com>
2024-02-25 15:47:59 -0400

    Update app.component.ts

--- angularwithasp.client/src/app/app.component.ts
+++ angularwithasp.client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,18 @@ import { Component, OnInit, inject } from '@angular/core';
 })
 export class AppComponent {
   title = 'eCommerce-app'
-  http = inject(HttpClient); // voy a usar el httpclient
+  http = inject(HttpClient); // voy a usar el httpclient patron de injeccion de dependencias
+  products: Product[] = [];
   
   changeTitle() {
     this.title = 'Nombre Cambiado';
+  }
+
+  ngOnInit() { //Es para hacer algo con mi componente cuando ya esta renderizado
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products') //Hacer una peticio, llamo a la api fake de platzi <Product[]> significa que va a recibir un array de Product
+    .subscribe((data) => { //subscribe es algo similar a resolver una promesa. Data es un array de product, lista de productos
+      this.products = data;
+    });
+
   }
 }
